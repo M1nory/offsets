e1c5e624c4af592f3f487894d0d5ae5db37187e2
M1nory <78639249+M1nory@users.noreply.github.com>
2025-09-10 08:08:51 +0200

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -3,7 +3,7 @@ namespace RBX {
         enum DataModel : uint32_t {
             GameID = 0x198,
             PlaceID = 0x1A0,
-            FakeDataModelPointer = 0x7097018,
+            FakeDataModelPointer = 0x70b1ef8,
             FakeDataModelToDataModel = 0x1c0,
             DataModelToRenderView1 = 0x1D8,
             DataModelToRenderView2 = 0x8,
